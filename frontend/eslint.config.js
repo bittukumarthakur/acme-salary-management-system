f330e39e3b1d96736b3338d53e381c0cab24ff54
Bittu Kumar Thakur <bittukumart26@gmail.com>
2026-06-27 17:23:16 +0530

    chore(frontend): align test typing and ignore yarn artifacts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,7 +7,7 @@ import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'coverage']),
+  globalIgnores(['dist', 'coverage', '.yarn']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
